91d7e5e32e65e5f140321ce72fbcd2000ac190ef
Null-Sector2025 <2734952662@qq.com>
2026-07-03 22:21:42 +0800

    通用 Lua 头文件引用（vcpkg 兼容）

--- engine/script/lua_bind.h
+++ engine/script/lua_bind.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <string>
+
+extern "C" {
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
+}
 
 namespace MoLin {
 
@@ -10,13 +13,10 @@ class LuaBind {
 public:
     LuaBind();
     ~LuaBind();
-
     bool DoFile(const std::string& filename);
     bool DoString(const std::string& code);
     lua_State* GetState() { return L; }
-
     void RegisterCoreFunctions();
-
 private:
     lua_State* L = nullptr;
 };
