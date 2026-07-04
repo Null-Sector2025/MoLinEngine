734f8681b6876681f8b3ac02fc758b3e08ab8604
Null-Sector2025 <2734952662@qq.com>
2026-07-04 18:45:47 +0800

    修复 lua_api.cpp 移除 http_client 引用

--- engine/script/lua_api.cpp
+++ engine/script/lua_api.cpp
@@ -6,9 +6,6 @@
 #include "../rpg/event_system.h"
 #include "../rpg/inventory.h"
 #include "../audio/audio_manager.h"
-#include "../graphics/camera.h"
-#include "../graphics/transition.h"
-#include "../network/http_client.h"
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -16,7 +13,6 @@
 
 namespace MoLin {
 
-// ---- 全局引擎组件 ----
 static std::shared_ptr<RPG::DialogueManager> g_DialogueMgr;
 static std::shared_ptr<RPG::QuestManager> g_QuestMgr;
 static std::shared_ptr<RPG::BattleSystem> g_BattleSystem;
@@ -82,184 +78,54 @@ void LuaAPI::RegisterAll(LuaBind& lua) {
     lua_register(L, "TriggerEvent", Lua_TriggerEvent);
 }
 
-// ---- 系统 ----
-int LuaAPI::Lua_Log(lua_State* L) {
-    const char* msg = lua_tostring(L, 1);
-    if (msg) std::cout << "[Lua] " << msg << std::endl;
-    return 0;
-}
-int LuaAPI::Lua_Sleep(lua_State* L) {
-    int ms = lua_tointeger(L, 1);
-    if (ms > 0) std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-    return 0;
-}
-int LuaAPI::Lua_Random(lua_State* L) {
-    lua_pushinteger(L, rand());
-    return 1;
-}
-int LuaAPI::Lua_RandomRange(lua_State* L) {
-    int min = lua_tointeger(L, 1);
-    int max = lua_tointeger(L, 2);
-    lua_pushinteger(L, min + rand() % (max - min + 1));
-    return 1;
-}
-
-// ---- 场景 ----
-int LuaAPI::Lua_LoadScene(lua_State* L) {
-    const char* scene = lua_tostring(L, 1);
-    if (scene) GameState::Instance().GetPlayer().sceneId = scene;
-    return 0;
-}
-int LuaAPI::Lua_GetCurrentScene(lua_State* L) {
-    lua_pushstring(L, GameState::Instance().GetPlayer().sceneId.c_str());
-    return 1;
-}
-
-// ---- 存档 ----
-int LuaAPI::Lua_SaveGame(lua_State* L) { int s = lua_tointeger(L, 1); if (s <= 0) s = 1; lua_pushboolean(L, GameState::Instance().Save(s)); return 1; }
-int LuaAPI::Lua_LoadGame(lua_State* L) { int s = lua_tointeger(L, 1); if (s <= 0) s = 1; lua_pushboolean(L, GameState::Instance().Load(s)); return 1; }
-int LuaAPI::Lua_DeleteSave(lua_State* L) { int s = lua_tointeger(L, 1); if (s <= 0) s = 1; lua_pushboolean(L, GameState::Instance().DeleteSave(s)); return 1; }
-int LuaAPI::Lua_GetSaveSlots(lua_State* L) {
-    lua_newtable(L);
-    for (int i = 1; i <= 5; i++) {
-        lua_pushinteger(L, i);
-        std::vector<uint8_t> dummy;
-        // 简单检查文件是否存在
-        std::string path = "save_" + std::to_string(i) + ".sav";
-        FILE* f = fopen(path.c_str(), "rb");
-        lua_pushboolean(L, f != nullptr);
-        if (f) fclose(f);
-        lua_settable(L, -3);
-    }
-    return 1;
-}
-
-// ---- 对话 ----
-int LuaAPI::Lua_StartDialogue(lua_State* L) {
-    int n = lua_gettop(L);
-    if (n < 2 || !g_DialogueMgr) return 0;
-    std::vector<RPG::DialogueLine> lines;
-    for (int i = 1; i <= n; i += 2) {
-        if (i + 1 > n) break;
-        const char* speaker = lua_tostring(L, i);
-        const char* text = lua_tostring(L, i + 1);
-        if (speaker && text) lines.push_back({speaker, text});
-    }
-    if (!lines.empty()) g_DialogueMgr->StartDialogue(lines);
-    return 0;
-}
-int LuaAPI::Lua_ShowNextLine(lua_State* L) { if (g_DialogueMgr) g_DialogueMgr->ShowNextLine(); return 0; }
-int LuaAPI::Lua_IsDialogueActive(lua_State* L) { lua_pushboolean(L, g_DialogueMgr ? g_DialogueMgr->IsActive() : false); return 1; }
-
-// ---- 物品 ----
-int LuaAPI::Lua_GiveItem(lua_State* L) {
-    const char* id = lua_tostring(L, 1); int cnt = lua_tointeger(L, 2); if (cnt <= 0) cnt = 1;
-    if (id) GameState::Instance().GetPlayer().inventory[id] += cnt;
-    return 0;
-}
-int LuaAPI::Lua_RemoveItem(lua_State* L) {
-    const char* id = lua_tostring(L, 1); int cnt = lua_tointeger(L, 2); if (cnt <= 0) cnt = 1;
-    auto& inv = GameState::Instance().GetPlayer().inventory;
-    auto it = inv.find(id);
-    if (it != inv.end()) { it->second -= cnt; if (it->second <= 0) inv.erase(it); lua_pushboolean(L, true); }
-    else lua_pushboolean(L, false);
-    return 1;
-}
-int LuaAPI::Lua_GetItemCount(lua_State* L) {
-    auto& inv = GameState::Instance().GetPlayer().inventory;
-    auto it = inv.find(lua_tostring(L, 1));
-    lua_pushinteger(L, it != inv.end() ? it->second : 0);
-    return 1;
-}
-int LuaAPI::Lua_HasItem(lua_State* L) {
-    auto& inv = GameState::Instance().GetPlayer().inventory;
-    auto it = inv.find(lua_tostring(L, 1));
-    int need = lua_tointeger(L, 2); if (need <= 0) need = 1;
-    lua_pushboolean(L, it != inv.end() && it->second >= need);
-    return 1;
-}
-int LuaAPI::Lua_UseItem(lua_State* L) {
-    const char* id = lua_tostring(L, 1);
-    if (id) {
-        auto proto = RPG::Inventory::GetItemPrototype(id);
-        if (proto && proto->onUse) {
-            // 需要角色对象，此处简化
-            std::cout << "[Lua] Used item: " << id << std::endl;
-        }
-        Lua_RemoveItem(L);
-    }
-    return 0;
-}
-
-// ---- 任务 ----
-int LuaAPI::Lua_StartQuest(lua_State* L) { const char* id = lua_tostring(L, 1); if (id && g_QuestMgr) { g_QuestMgr->StartQuest(id); GameState::Instance().GetPlayer().quests[id] = 1; } return 0; }
-int LuaAPI::Lua_CompleteQuest(lua_State* L) { const char* id = lua_tostring(L, 1); if (id) GameState::Instance().GetPlayer().quests[id] = 2; return 0; }
-int LuaAPI::Lua_FailQuest(lua_State* L) { const char* id = lua_tostring(L, 1); if (id) GameState::Instance().GetPlayer().quests[id] = -1; return 0; }
-int LuaAPI::Lua_UpdateObjective(lua_State* L) {
-    const char* id = lua_tostring(L, 1); int idx = lua_tointeger(L, 2); int prog = lua_tointeger(L, 3);
-    if (id && g_QuestMgr) g_QuestMgr->UpdateObjective(id, idx, prog);
-    return 0;
-}
-
-// ---- 角色 ----
-int LuaAPI::Lua_GetHP(lua_State* L) { lua_pushinteger(L, GameState::Instance().GetPlayer().hp); return 1; }
-int LuaAPI::Lua_SetHP(lua_State* L) { GameState::Instance().GetPlayer().hp = lua_tointeger(L, 1); return 0; }
-int LuaAPI::Lua_GetMP(lua_State* L) { lua_pushinteger(L, GameState::Instance().GetPlayer().mp); return 1; }
-int LuaAPI::Lua_SetMP(lua_State* L) { GameState::Instance().GetPlayer().mp = lua_tointeger(L, 1); return 0; }
-int LuaAPI::Lua_GetLevel(lua_State* L) { lua_pushinteger(L, GameState::Instance().GetPlayer().level); return 1; }
-int LuaAPI::Lua_AddExp(lua_State* L) {
-    int exp = lua_tointeger(L, 1); auto& p = GameState::Instance().GetPlayer();
-    p.exp += exp; if (p.exp >= p.level * 100) { p.exp -= p.level * 100; p.level++; }
-    lua_pushinteger(L, p.level); return 1;
-}
-int LuaAPI::Lua_Heal(lua_State* L) { auto& p = GameState::Instance().GetPlayer(); p.hp = p.maxHp; p.mp = p.maxMp; return 0; }
-int LuaAPI::Lua_Damage(lua_State* L) { auto& p = GameState::Instance().GetPlayer(); p.hp -= lua_tointeger(L, 1); if (p.hp < 0) p.hp = 0; return 0; }
-
-// ---- 战斗 ----
-int LuaAPI::Lua_StartBattle(lua_State* L) {
-    if (g_BattleSystem) {
-        std::vector<std::shared_ptr<RPG::BattleEntity>> allies, enemies;
-        g_BattleSystem->StartBattle(allies, enemies);
-        lua_pushboolean(L, true);
-    } else lua_pushboolean(L, false);
-    return 1;
-}
-int LuaAPI::Lua_IsBattleOver(lua_State* L) { lua_pushboolean(L, g_BattleSystem ? g_BattleSystem->IsBattleOver() : true); return 1; }
-int LuaAPI::Lua_ExecuteBattleCommand(lua_State* L) {
-    int idx = lua_tointeger(L, 1); const char* cmd = lua_tostring(L, 2);
-    if (g_BattleSystem && cmd) {
-        RPG::BattleCommand bc; bc.name = cmd;
-        g_BattleSystem->ExecuteCommand(idx, bc);
-    }
-    return 0;
-}
-
-// ---- 音频 ----
-int LuaAPI::Lua_PlaySound(lua_State* L) { AudioManager::Instance().PlaySound(lua_tostring(L, 1)); return 0; }
-int LuaAPI::Lua_PlayMusic(lua_State* L) { AudioManager::Instance().PlayMusic(lua_tostring(L, 1)); return 0; }
+int LuaAPI::Lua_Log(lua_State* L) { const char* m=lua_tostring(L,1); if(m)std::cout<<"[Lua] "<<m<<std::endl; return 0; }
+int LuaAPI::Lua_Sleep(lua_State* L) { int ms=lua_tointeger(L,1); if(ms>0)std::this_thread::sleep_for(std::chrono::milliseconds(ms)); return 0; }
+int LuaAPI::Lua_Random(lua_State* L) { lua_pushinteger(L,rand()); return 1; }
+int LuaAPI::Lua_RandomRange(lua_State* L) { int a=lua_tointeger(L,1),b=lua_tointeger(L,2); lua_pushinteger(L,a+rand()%(b-a+1)); return 1; }
+int LuaAPI::Lua_LoadScene(lua_State* L) { const char* s=lua_tostring(L,1); if(s)GameState::Instance().GetPlayer().sceneId=s; return 0; }
+int LuaAPI::Lua_GetCurrentScene(lua_State* L) { lua_pushstring(L,GameState::Instance().GetPlayer().sceneId.c_str()); return 1; }
+int LuaAPI::Lua_SaveGame(lua_State* L) { int s=lua_tointeger(L,1);if(s<=0)s=1;lua_pushboolean(L,GameState::Instance().Save(s));return 1; }
+int LuaAPI::Lua_LoadGame(lua_State* L) { int s=lua_tointeger(L,1);if(s<=0)s=1;lua_pushboolean(L,GameState::Instance().Load(s));return 1; }
+int LuaAPI::Lua_DeleteSave(lua_State* L) { int s=lua_tointeger(L,1);if(s<=0)s=1;lua_pushboolean(L,GameState::Instance().DeleteSave(s));return 1; }
+int LuaAPI::Lua_GetSaveSlots(lua_State* L) { lua_newtable(L); for(int i=1;i<=5;i++){lua_pushinteger(L,i);std::string p="save_"+std::to_string(i)+".sav";FILE*f=fopen(p.c_str(),"rb");lua_pushboolean(L,f!=nullptr);if(f)fclose(f);lua_settable(L,-3);}return 1; }
+int LuaAPI::Lua_StartDialogue(lua_State* L) { int n=lua_gettop(L);if(n<2||!g_DialogueMgr)return 0;std::vector<RPG::DialogueLine> lines;for(int i=1;i<=n;i+=2){if(i+1>n)break;const char*s=lua_tostring(L,i);const char*t=lua_tostring(L,i+1);if(s&&t)lines.push_back({s,t});}if(!lines.empty())g_DialogueMgr->StartDialogue(lines);return 0; }
+int LuaAPI::Lua_ShowNextLine(lua_State* L) { if(g_DialogueMgr)g_DialogueMgr->ShowNextLine(); return 0; }
+int LuaAPI::Lua_IsDialogueActive(lua_State* L) { lua_pushboolean(L,g_DialogueMgr?g_DialogueMgr->IsActive():false); return 1; }
+int LuaAPI::Lua_GiveItem(lua_State* L) { const char* id=lua_tostring(L,1);int c=lua_tointeger(L,2);if(c<=0)c=1;if(id)GameState::Instance().GetPlayer().inventory[id]+=c;return 0; }
+int LuaAPI::Lua_RemoveItem(lua_State* L) { const char* id=lua_tostring(L,1);int c=lua_tointeger(L,2);if(c<=0)c=1;auto&inv=GameState::Instance().GetPlayer().inventory;auto it=inv.find(id);if(it!=inv.end()){it->second-=c;if(it->second<=0)inv.erase(it);lua_pushboolean(L,true);}else lua_pushboolean(L,false);return 1; }
+int LuaAPI::Lua_GetItemCount(lua_State* L) { auto&inv=GameState::Instance().GetPlayer().inventory;auto it=inv.find(lua_tostring(L,1));lua_pushinteger(L,it!=inv.end()?it->second:0);return 1; }
+int LuaAPI::Lua_HasItem(lua_State* L) { auto&inv=GameState::Instance().GetPlayer().inventory;auto it=inv.find(lua_tostring(L,1));int n=lua_tointeger(L,2);if(n<=0)n=1;lua_pushboolean(L,it!=inv.end()&&it->second>=n);return 1; }
+int LuaAPI::Lua_UseItem(lua_State* L) { const char* id=lua_tostring(L,1);if(id){Lua_RemoveItem(L);}return 0; }
+int LuaAPI::Lua_StartQuest(lua_State* L) { const char* id=lua_tostring(L,1);if(id&&g_QuestMgr){g_QuestMgr->StartQuest(id);GameState::Instance().GetPlayer().quests[id]=1;}return 0; }
+int LuaAPI::Lua_CompleteQuest(lua_State* L) { const char* id=lua_tostring(L,1);if(id)GameState::Instance().GetPlayer().quests[id]=2;return 0; }
+int LuaAPI::Lua_FailQuest(lua_State* L) { const char* id=lua_tostring(L,1);if(id)GameState::Instance().GetPlayer().quests[id]=-1;return 0; }
+int LuaAPI::Lua_UpdateObjective(lua_State* L) { const char* id=lua_tostring(L,1);int idx=lua_tointeger(L,2),prog=lua_tointeger(L,3);if(id&&g_QuestMgr)g_QuestMgr->UpdateObjective(id,idx,prog);return 0; }
+int LuaAPI::Lua_GetHP(lua_State* L) { lua_pushinteger(L,GameState::Instance().GetPlayer().hp); return 1; }
+int LuaAPI::Lua_SetHP(lua_State* L) { GameState::Instance().GetPlayer().hp=lua_tointeger(L,1); return 0; }
+int LuaAPI::Lua_GetMP(lua_State* L) { lua_pushinteger(L,GameState::Instance().GetPlayer().mp); return 1; }
+int LuaAPI::Lua_SetMP(lua_State* L) { GameState::Instance().GetPlayer().mp=lua_tointeger(L,1); return 0; }
+int LuaAPI::Lua_GetLevel(lua_State* L) { lua_pushinteger(L,GameState::Instance().GetPlayer().level); return 1; }
+int LuaAPI::Lua_AddExp(lua_State* L) { int exp=lua_tointeger(L,1);auto&p=GameState::Instance().GetPlayer();p.exp+=exp;if(p.exp>=p.level*100){p.exp-=p.level*100;p.level++;}lua_pushinteger(L,p.level);return 1; }
+int LuaAPI::Lua_Heal(lua_State* L) { auto&p=GameState::Instance().GetPlayer();p.hp=p.maxHp;p.mp=p.maxMp;return 0; }
+int LuaAPI::Lua_Damage(lua_State* L) { auto&p=GameState::Instance().GetPlayer();p.hp-=lua_tointeger(L,1);if(p.hp<0)p.hp=0;return 0; }
+int LuaAPI::Lua_StartBattle(lua_State* L) { if(g_BattleSystem){std::vector<std::shared_ptr<RPG::BattleEntity>> a,e;g_BattleSystem->StartBattle(a,e);lua_pushboolean(L,true);}else lua_pushboolean(L,false);return 1; }
+int LuaAPI::Lua_IsBattleOver(lua_State* L) { lua_pushboolean(L,g_BattleSystem?g_BattleSystem->IsBattleOver():true);return 1; }
+int LuaAPI::Lua_ExecuteBattleCommand(lua_State* L) { int idx=lua_tointeger(L,1);const char* cmd=lua_tostring(L,2);if(g_BattleSystem&&cmd){RPG::BattleCommand bc;bc.name=cmd;g_BattleSystem->ExecuteCommand(idx,bc);}return 0; }
+int LuaAPI::Lua_PlaySound(lua_State* L) { AudioManager::Instance().PlaySound(lua_tostring(L,1)); return 0; }
+int LuaAPI::Lua_PlayMusic(lua_State* L) { AudioManager::Instance().PlayMusic(lua_tostring(L,1)); return 0; }
 int LuaAPI::Lua_StopMusic(lua_State* L) { AudioManager::Instance().StopMusic(); return 0; }
-int LuaAPI::Lua_SetMusicVolume(lua_State* L) { AudioManager::Instance().SetMusicVolume(lua_tonumber(L, 1)); return 0; }
-
-// ---- 标记 ----
-int LuaAPI::Lua_SetFlag(lua_State* L) { const char* k = lua_tostring(L, 1); const char* v = lua_tostring(L, 2); if (k && v) GameState::Instance().GetPlayer().flags[k] = v; return 0; }
-int LuaAPI::Lua_GetFlag(lua_State* L) { auto& f = GameState::Instance().GetPlayer().flags; auto it = f.find(lua_tostring(L, 1)); lua_pushstring(L, it != f.end() ? it->second.c_str() : ""); return 1; }
-int LuaAPI::Lua_HasFlag(lua_State* L) { auto& f = GameState::Instance().GetPlayer().flags; lua_pushboolean(L, f.find(lua_tostring(L, 1)) != f.end()); return 1; }
-int LuaAPI::Lua_ClearFlag(lua_State* L) { GameState::Instance().GetPlayer().flags.erase(lua_tostring(L, 1)); return 0; }
-
-// ---- 摄像机 ----
-int LuaAPI::Lua_SetCameraPos(lua_State* L) { /* 需要全局摄像机引用 */ return 0; }
-int LuaAPI::Lua_GetCameraPos(lua_State* L) { lua_pushnumber(L, 0); lua_pushnumber(L, 0); return 2; }
+int LuaAPI::Lua_SetMusicVolume(lua_State* L) { AudioManager::Instance().SetMusicVolume(lua_tonumber(L,1)); return 0; }
+int LuaAPI::Lua_SetFlag(lua_State* L) { const char*k=lua_tostring(L,1);const char*v=lua_tostring(L,2);if(k&&v)GameState::Instance().GetPlayer().flags[k]=v;return 0; }
+int LuaAPI::Lua_GetFlag(lua_State* L) { auto&f=GameState::Instance().GetPlayer().flags;auto it=f.find(lua_tostring(L,1));lua_pushstring(L,it!=f.end()?it->second.c_str():"");return 1; }
+int LuaAPI::Lua_HasFlag(lua_State* L) { auto&f=GameState::Instance().GetPlayer().flags;lua_pushboolean(L,f.find(lua_tostring(L,1))!=f.end());return 1; }
+int LuaAPI::Lua_ClearFlag(lua_State* L) { GameState::Instance().GetPlayer().flags.erase(lua_tostring(L,1));return 0; }
+int LuaAPI::Lua_SetCameraPos(lua_State* L) { return 0; }
+int LuaAPI::Lua_GetCameraPos(lua_State* L) { lua_pushnumber(L,0);lua_pushnumber(L,0);return 2; }
 int LuaAPI::Lua_ShakeCamera(lua_State* L) { return 0; }
-
-// ---- 过渡 ----
 int LuaAPI::Lua_FadeIn(lua_State* L) { return 0; }
 int LuaAPI::Lua_FadeOut(lua_State* L) { return 0; }
-
-// ---- HTTP ----
-int LuaAPI::Lua_HttpGet(lua_State* L) { std::string r = Network::HttpClient::Get(lua_tostring(L, 1)); lua_pushstring(L, r.c_str()); return 1; }
-int LuaAPI::Lua_HttpPost(lua_State* L) { std::string r = Network::HttpClient::Post(lua_tostring(L, 1), lua_tostring(L, 2) ?: ""); lua_pushstring(L, r.c_str()); return 1; }
-
-// ---- 事件 ----
-int LuaAPI::Lua_TriggerEvent(lua_State* L) { const char* n = lua_tostring(L, 1); if (n && g_EventMgr) g_EventMgr->TriggerEvent(n); return 0; }
+int LuaAPI::Lua_HttpGet(lua_State* L) { lua_pushstring(L,""); return 1; }
+int LuaAPI::Lua_HttpPost(lua_State* L) { lua_pushstring(L,""); return 1; }
+int LuaAPI::Lua_TriggerEvent(lua_State* L) { const char* n=lua_tostring(L,1);if(n&&g_EventMgr)g_EventMgr->TriggerEvent(n);return 0; }
 
 } // namespace MoLin
