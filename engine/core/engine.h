0e105e0ba143f9db83e7d1314f4f9e4a6e6101b4
Null-Sector2025 <2734952662@qq.com>
2026-07-21 11:43:28 +0800

    更新 engine.h

--- engine/core/engine.h
+++ engine/core/engine.h
@@ -1,15 +1,12 @@
 #pragma once
-#include <string>
-#include <functional>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
+#include <functional>
+#include <string>
 
 namespace MoLin {
 
 struct WindowConfig {
-    std::string title = "墨麟引擎";
+    std::string title = "MoLinEngine";
     int width = 1280;
     int height = 720;
     bool fullscreen = false;
@@ -19,8 +16,8 @@ struct WindowConfig {
 
 class Engine {
 public:
-    using UpdateCallback = std::function<void(float)>;
-    using RenderCallback = std::function<void(SDL_Renderer*)>;
+    using UpdateCallback = std::function<void(float delta)>;
+    using RenderCallback = std::function<void()>;
     using EventCallback = std::function<void(const SDL_Event&)>;
 
     Engine();
@@ -31,35 +28,24 @@ public:
     void Shutdown();
     void Quit();
 
-    void SetUpdateCallback(UpdateCallback cb) { m_UpdateCB = cb; }
-    void SetRenderCallback(RenderCallback cb) { m_RenderCB = cb; }
-    void SetEventCallback(EventCallback cb) { m_EventCB = cb; }
+    void SetUpdateCallback(UpdateCallback cb);
+    void SetRenderCallback(RenderCallback cb);
+    void SetEventCallback(EventCallback cb);
 
-    SDL_Window* GetWindow() const { return m_Window; }
-    SDL_Renderer* GetRenderer() const { return m_Renderer; }
-
-    float GetFPS() const { return m_FPS; }
-    float GetDeltaTime() const { return m_DeltaTime; }
-    int GetWidth() const { return m_Config.width; }
-    int GetHeight() const { return m_Config.height; }
-
-    void SetClearColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255);
+    SDL_Window* GetWindow() const { return m_window; }
+    SDL_Renderer* GetRenderer() const { return m_renderer; }
+    float GetFPS() const { return m_fps; }
+    float GetDeltaTime() const { return m_deltaTime; }
 
 private:
-    WindowConfig m_Config;
-    SDL_Window* m_Window = nullptr;
-    SDL_Renderer* m_Renderer = nullptr;
-    bool m_Running = false;
-    float m_FPS = 0.0f;
-    float m_DeltaTime = 0.0f;
-    SDL_Color m_ClearColor = {0, 0, 0, 255};
-
-    UpdateCallback m_UpdateCB;
-    RenderCallback m_RenderCB;
-    EventCallback m_EventCB;
-
-    void HandleEvents();
-    void UpdateFPS(float delta);
+    SDL_Window* m_window = nullptr;
+    SDL_Renderer* m_renderer = nullptr;
+    UpdateCallback m_updateCallback;
+    RenderCallback m_renderCallback;
+    EventCallback m_eventCallback;
+    bool m_running = false;
+    float m_fps = 0.0f;
+    float m_deltaTime = 0.0f;
 };
 
 } // namespace MoLin
